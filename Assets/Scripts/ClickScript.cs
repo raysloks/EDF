d288f62be2c89ea9140bf80eeb905511ba6affba
Emil Bertilsson <raysloks@gmail.com>
2016-02-05 23:14:56 +0100

    Created Unity project

---
+++ Assets/Scripts/ClickScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            transform.Rotate(new Vector3(0.0f, 180.0f, 00.0f));
+    }
+}
