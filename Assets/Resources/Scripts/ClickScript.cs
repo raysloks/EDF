87a5c0f60978a568fb14140f7526f2ada7c44979
Emil Bertilsson <raysloks@gmail.com>
2016-02-07 19:21:09 +0100

    Added turn manager

---
+++ Assets/Resources/Scripts/ClickScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickScript : MonoBehaviour {
+
+    public Animator anim;
+
+    public Vector2 target;
+    float speed = 4.0f;
+
+    public bool my_turn;
+    public float advance;
+
+	// Use this for initialization
+	void Start()
+    {
+        GameObject obj = Instantiate(Resources.Load("Prefabs/Shadow")) as GameObject;
+        ShadowScript ss = obj.GetComponent<ShadowScript>();
+        ss.follow = transform;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (my_turn)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit rh = new RaycastHit();
+                if (Physics.Raycast(ray, out rh))
+                {
+                    target = rh.point;
+
+                    target.x = Mathf.Round(target.x - 0.5f) + 0.5f;
+                    target.y = Mathf.Round(target.y - 0.5f) + 0.5f;
+
+                    Vector2 dif = target - new Vector2(transform.position.x, transform.position.y);
+                    if (dif.magnitude > 5)
+                        target = transform.position;
+                    else
+                        my_turn = false;
+                }
+            }
+        }
+
+        Vector2 delta = target - new Vector2(transform.position.x, transform.position.y);
+
+        anim.SetBool("walking", delta != new Vector2());
+
+        if (delta == new Vector2() && !my_turn)
+            advance = 1.0f;
+
+        if (delta.x > 0.0f)
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        if (delta.x < 0.0f)
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        float max_len = speed * Time.deltaTime;
+        if (delta.magnitude > max_len)
+            delta = delta.normalized * max_len;
+        transform.Translate(delta, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            transform.Translate(new Vector3(1.0f, 0.0f, 0.0f));
+    }
+}
---
+++ Assets/Resources/Scripts/TurnManagerScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TurnManagerScript : MonoBehaviour {
+
+    public CameraFollowScript cam;
+
+    Dictionary<float, ClickScript> order;
+    Dictionary<float, ClickScript> new_order;
+
+    ClickScript current_turnholder;
+
+	// Use this for initialization
+	void Start () {
+        order = new Dictionary<float, ClickScript>();
+        new_order = new Dictionary<float, ClickScript>();
+
+        order.Add(0.0f, (Instantiate(Resources.Load("Prefabs/Character")) as GameObject).GetComponent<ClickScript>());
+        order.Add(0.5f, (Instantiate(Resources.Load("Prefabs/Character")) as GameObject).GetComponent<ClickScript>());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (current_turnholder == null)
+        {
+            var nume = order.GetEnumerator();
+            if (nume.MoveNext())
+            {
+                var next = nume.Current;
+                current_turnholder = next.Value;
+                float time_to_advance = next.Key;
+                while (nume.MoveNext())
+                    new_order.Add(nume.Current.Key - time_to_advance, nume.Current.Value);
+                current_turnholder.my_turn = true;
+                current_turnholder.advance = 0.0f;
+            }
+        }
+        if (current_turnholder != null)
+        {
+            cam.target = current_turnholder;
+            if (current_turnholder.advance>0.0f)
+            {
+                current_turnholder.my_turn = false;
+                new_order.Add(current_turnholder.advance, current_turnholder);
+                order = new_order;
+                new_order = new Dictionary<float, ClickScript>();
+                current_turnholder = null;
+            }
+        }
+	}
+}
